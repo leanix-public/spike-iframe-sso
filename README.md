18b9d7fbf2738745eda9b14e9f9ca13d15846dff
Paulo Santos <paulo.santos@leanix.net>
2022-11-21 19:18:46 -0300

    current state

---
+++ README.md
@@ -0,0 +1,4 @@
+#
+https://leanix-public.github.io/spike-iframe-sso/
+
+https://github.com/leanix-public/spike-iframe-sso
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [basicSsl()]
+  plugins: [],
+  base: '/spike-iframe-sso/'
 })
